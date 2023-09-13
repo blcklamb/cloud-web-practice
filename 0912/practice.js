9a5b5556c0332ee744a71bb4410da667f1aa2211
blcklamb <whenucan35@gmail.com>
2023-09-13 14:20:29 +0900

    add: supplement code block - class for day 09.12

--- 0912/practice.js
+++ 0912/practice.js
@@ -77,3 +77,63 @@ const getBusFare03 = (age) => {
 const getBusFare04 = (age) => {
   [0, 8, 14, 20, 70];
 };
+
+// 보충
+/* while */
+const consoleInWhile = () => {
+  let i = 1;
+  while (i <= 10) {
+    console.log(`The number i is ${i}`);
+    i++;
+  }
+
+  let j = 1;
+  do {
+    console.log(`The number j is ${j}`);
+    j++;
+  } while (j <= 10);
+
+  let price = 17_000;
+  let coinUnit = 500;
+  let coinCount = 0;
+
+  while (price > 0) {
+    price -= coinUnit;
+    coinCount++;
+  }
+  console.log(`반환된 500원 갯수: ${coinCount}`);
+};
+consoleInWhile();
+
+/* Control */
+const consoleControl = () => {
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+      console.log(`현재 값이 ${i}이므로 종료합니다.`);
+      break;
+    }
+    console.log(`현재 i 값: ${i}`);
+  }
+
+  for (let j = 0; j < 10; j++) {
+    if (3 < j && j <= 5) {
+      continue;
+    }
+    console.log(`현재 j 값: ${j}`);
+  }
+};
+
+consoleControl();
+
+/* Function */
+const add = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiple = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const calculator = (a, operator, b) => {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return minus(a, b);
+  if (operator === "/") return multiple(a, b);
+  if (operator === "*") return divide(a, b);
+};
