c8842bd5b84ed67b5870bbf6516640b7d67ca73c
blcklamb <whenucan35@gmail.com>
2023-09-18 15:50:45 +0900

    delete: useless file

--- 0918/helloworld-app/src/index.js
+++ 0918/helloworld-app/src/index.js
@@ -1,25 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const pStyle = { color: "white", backgroundColor: "teal" };
-const insertElement = React.createElement(
-  "p",
-  { style: pStyle },
-  "JSX를 이용하지 않고 새로운 element 추가"
-);
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  insertElement
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
