cb832f231477344255f02ce46e3e09e6e2385328
blcklamb <whenucan35@gmail.com>
2023-09-18 11:33:02 +0900

    check: append element without JSX

--- 0918/helloworld-app/src/index.js
+++ 0918/helloworld-app/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const insertElement = React.createElement(
+  "p",
+  {},
+  "JSX를 이용하지 않고 새로운 element 추가"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  insertElement
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
