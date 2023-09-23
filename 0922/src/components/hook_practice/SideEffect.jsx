7dcdf513a9315a6d6d7ea0b4eec5a2cb2d355d2a
blcklamb <whenucan35@gmail.com>
2023-09-23 10:35:19 +0900

    add: <Side Effect> practice - DOM manipulate by setInterval and css fontsize

--- 0922/src/components/hook_practice/SideEffect.jsx
+++ 0922/src/components/hook_practice/SideEffect.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 export const SideEffect = ({ message }) => {
   const calculatedMessage = `${message}은/는 정말 재밌어요`;
   // ! 직접 DOM을 조작
   document.title = "하하";
 
   const [fontSize, setFontSize] = useState(10);
+  const fontRef = useRef(10);
   // ! 직접 DOM을 조작
-  setInterval(() => {
-    setFontSize(fontSize + 2);
-  }, 3000);
+  useEffect(() => {
+    setInterval(() => {
+      console.log("ho");
+      setFontSize((fontRef.current += 2));
+    }, 4000);
+  }, []);
+
+  // setInterval(() => {
+  //   setFontSize((prev) => prev + 2);
+  // }, 3000);
 
   return (
     <>
